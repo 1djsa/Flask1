45921e8e61c181fcf2bb98d23f6ebaf0144e20f8
1djsa <sammysibero@gmail.com>
2024-06-01 14:48:23 +0700

    Added mine.txt

--- main.py
+++ main.py
@@ -4,27 +4,36 @@ import os
 
 app = Flask(__name__)
 
-@app.route("/ALAMAT")
-def FUNCTION_NAME():
-    return "<hi> whats up !"
-
 # Home/1st page
 @app.route("/")
 def pertama():
-    # <a> anchor tag with its attribute
-    return "<h1>Hello, World!</h1><br><p>Nice to see you</p><p>please check</p><a href='/random_fact'>View a random fact!</a><br><a href='/modern_fact'>View a modern fact!</a>"
-    
+    return """<h1>Hello, World!</h1><br>
+              <p>Nice to see you</p>
+              <p>please check</p>
+              <a href='/random_fact'>View a random fact!</a><br>
+              <a href='/modern_fact'>View a modern fact!</a><br>
+              <a href='/my_self'>Fun Fact about myself!</a>"""
+
 # 2nd page
 @app.route("/random_fact")
-
 def kedua():
     txt_name = random.choice(os.listdir("fact_list"))
-    # formatted string
     with open(f'fact_list/{txt_name}', 'r') as f:
         document = f.read()
     return f'{document}'
+
 # 2nd page
 @app.route('/modern_fact')
 def index():
-    return render_template('dasar.html')#folder templates
-app.run(debug=True)
+    return render_template('dasar.html')  # folder templates
+
+@app.route('/my_self')
+def ketiga():
+    text_name = (os.listdir("myself"))
+    with open(f'myself/{text_name}', 'r') as f:
+        midocument = f.read()
+    return f'{midocument}'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
